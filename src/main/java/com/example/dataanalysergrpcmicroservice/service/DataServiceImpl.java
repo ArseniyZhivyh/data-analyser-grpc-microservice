bf10564c3fd3755e403696d8dfc6e74de0df4ae7
Живых Арсений <ars14011998ars@yandex.ru>
2024-10-28 19:01:45 +0500

    finished

---
+++ src/main/java/com/example/dataanalysergrpcmicroservice/service/DataServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.dataanalysergrpcmicroservice.service;
+
+import com.example.dataanalysergrpcmicroservice.model.Data;
+import com.example.dataanalysergrpcmicroservice.repository.DataRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+ class DataServiceImpl  implements DataService {
+
+    private final DataRepository dataRepository;
+
+    @Override
+    public void handle (Data data) {
+        log.info("Data object {} was saved", data);
+    }
+
+    @Override
+    public List<Data> getWithBatch(long batchSize) {
+        List<Data> data = dataRepository.findAllWithOffset(batchSize);
+        if(data.size() > 0) {
+            dataRepository.incrementOffset(Long.min(batchSize, data.size()));
+        }
+        return data;
+    }
+
+}
